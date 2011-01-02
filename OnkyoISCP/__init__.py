1bc9f3fb4d9ff558eb7c0b4825751c926de4a3f5
Alexander Hartmaier <alex.hartmaier@gmail.com>
2011-01-02 14:00:46 +0100

    version 0.03

--- OnkyoISCP/__init__.py
+++ OnkyoISCP/__init__.py
@@ -4,7 +4,7 @@ import socket
 eg.RegisterPlugin(
     name = "Onkyo ISCP",
     author = "Alexander Hartmaier",
-    version = "0.02",
+    version = "0.03",
     kind = "external",
     description = "Controls any Onkyo Receiver which supports the ISCP protocol."
 )
